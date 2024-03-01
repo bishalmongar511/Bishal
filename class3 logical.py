5dbe3cd6751579ee5a96881e541952002bd91f92
unknown <02230260.cst@rub.edu.bt>
2024-03-01 14:12:28 +0600

    change to class3 logical.py

---
+++ class3 logical.py
@@ -0,0 +1,24 @@
+# Example 1
+x = 5
+print(x > 3 and x < 10) # True bexause both condition are True
+
+# Example 2
+y = 12
+print(y = 10 and y % 5 ==0) # False because the second condition is False
+
+# Example 1
+x = 5
+print(x < 3 or x > 10) # False bexause both condition are False
+
+# Example 2
+y = 12
+print(Y < 10 and Y % 2 ==0) # True because the second condition is True
+
+# Example 1
+x = 5
+print(not(x > 3 and x < 10)) # False bexause the condition inside the not is True
+
+# Example 2
+y = 12
+print(not(Y > 10 and Y % 5 == 0)) # Truebecause the condition inside the not is False
+
