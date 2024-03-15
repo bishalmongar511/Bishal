1861c7a01895eeab1aec61c697d2d4768e1b14e1
unknown <02230260.cst@rub.edu.bt>
2024-03-15 13:29:04 +0600

    change to browserhistory.py

---
+++ activity.py
@@ -0,0 +1,10 @@
+# Initilization empty lists and dictiomary to store stutend data.
+students_list = []
+students_dict = {}
+
+# Add student information
+name = input("Enter student's name: ")
+age = input("Enter student's age: ")
+garde = input("Enter student's grade: ")
+students_list.append(name)
+
