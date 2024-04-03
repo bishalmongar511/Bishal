4f56a0900a78d585bdaef75096fae45a06b8eacb
unknown <02230260.cst@rub.edu.bt>
2024-04-03 20:44:43 +0600

    chnage to sort_search.py

---
+++ bishalpractise.py
@@ -0,0 +1,10 @@
+Age = int(input("Enter your age: "))
+
+if Age < 18:
+    print("you can not vote. ")
+if Age < 10:
+    print("you will get chocolate.")
+if Age == 17:
+    print("next year your are eligiable.")
+else: 
+    print(" YOU ARE WELCOME! \n you can vote. ")
